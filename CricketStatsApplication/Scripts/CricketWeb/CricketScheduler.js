c40cc324aaf81b76ad72a8ad2f9eb3417807be1f
viveksingh407 <viveksingh407@hotmail.com>
2016-03-23 06:22:15 -0700

    Added Angular references and bootstrap to view logic.

---
+++ CricketStatsApplication/Scripts/CricketWeb/CricketScheduler.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    angular
+        .module('CricketApp', [])
+        .controller("CricketTeamsListCtrl", GetTeamsData);
+
+    function GetTeamsData() {
+        this.Teams = [{
+            teamId: 1,
+            teamName: 'Team1'
+        }, {
+            teamId: 2,
+            teamName: 'Team2'
+        }, {
+            teamId: 3,
+            teamName: 'Team3'
+        }, {
+            teamId: 4,
+            teamName: 'Team4'
+        }];
+    }
+})();
