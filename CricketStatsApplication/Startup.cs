be8cbc5bcf3bd07af957920f02a48b3bf802c6a8
viveksingh407 <viveksingh407@hotmail.com>
2016-03-26 14:33:09 -0700

    Removed additional files.

--- CricketStatsApplication/Startup.cs
+++ CricketStatsApplication/Startup.cs
@@ -1,14 +1,7 @@
-﻿using Microsoft.Owin;
-using Owin;
-
-[assembly: OwinStartupAttribute(typeof(CricketStatsApplication.Startup))]
+﻿
 namespace CricketStatsApplication
 {
     public partial class Startup
     {
-        public void Configuration(IAppBuilder app)
-        {
-            ConfigureAuth(app);
-        }
     }
 }
