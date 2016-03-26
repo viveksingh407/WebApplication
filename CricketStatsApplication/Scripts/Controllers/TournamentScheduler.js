4bcaabfe3c2815a50d4587d587a07e112d89be0b
viveksingh407 <viveksingh407@hotmail.com>
2016-03-26 15:37:56 -0700

    Updated controllers

--- CricketStatsApplication/Scripts/Controllers/TournamentScheduler.js
+++ CricketStatsApplication/Scripts/Controllers/TournamentScheduler.js
@@ -1,1 +1,17 @@
-﻿
+﻿var cricketAppController = function ($scope) {
+    $scope.Teams = [{
+        teamId: 1,
+        teamName: 'Team1'
+    }, {
+        teamId: 2,
+        teamName: 'Team2'
+    }, {
+        teamId: 3,
+        teamName: 'Team3'
+    }, {
+        teamId: 4,
+        teamName: 'Team4'
+    }];
+}
+
+cricketAppController.$inject = ['$scope']
--- CricketStatsApplication/Scripts/CricketTournamentApp.js
+++ CricketStatsApplication/Scripts/CricketTournamentApp.js
@@ -1,21 +1,3 @@
-﻿(function () {
-    angular
-        .module('CricketApp', [])
-        .controller("CricketTeamsListCtrl", GetTeamsData);
+﻿var cricketModule = angular.module('CricketApp', []);
 
-    function GetTeamsData() {
-        this.Teams = [{
-            teamId: 1,
-            teamName: 'Team1'
-        }, {
-            teamId: 2,
-            teamName: 'Team2'
-        }, {
-            teamId: 3,
-            teamName: 'Team3'
-        }, {
-            teamId: 4,
-            teamName: 'Team4'
-        }];
-    }
-})();
+cricketModule.controller("TournamentSchedulerCtrl", cricketAppController)
